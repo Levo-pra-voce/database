347ae10205812bc2f54e30eb3fd2992238d82ca7
luiz-eduardo14 <masterdudu123@hotmail.com>
2023-10-04 19:38:50 -0300

    first commit

---
+++ inicial_structure.sql
@@ -0,0 +1,105 @@
+create table usuario (
+    id bigserial primary key,
+    primeiro_nome text,
+    sobrenome text,
+    contato text,
+    email text,
+    data_criacao timestamp default now(),
+    ativo boolean
+);
+
+
+create table grupo (
+    id bigserial primary key,
+    nome text,
+    data_criacao timestamp default now(),
+    ativo boolean
+);
+
+create table usuario_grupo (
+    id_usuario bigint,
+    id_grupo bigint,
+    data_criacao timestamp default now(),
+    ativo boolean,
+    primary key (id_usuario, id_grupo),
+    foreign key (id_usuario) references usuario(id),
+    foreign key (id_grupo) references grupo(id)
+);
+
+create table mensagem (
+    id bigserial primary key,
+    id_usuario bigint not null,
+    id_grupo bigint not null,
+    id_mensagem_resposta bigint,
+    mensagem bytea not null,
+    tipo_mensagem text,
+    data_criacao timestamp default now(),
+    ativo boolean,
+    foreign key (id_usuario) references usuario(id),
+    foreign key (id_grupo) references grupo(id)
+);
+
+create table permissao (
+    id bigserial primary key,
+    nome text,
+    data_criacao timestamp default now(),
+    ativo boolean
+);
+
+create table perfil (
+    id bigserial primary key,
+    nome text,
+    data_criacao timestamp default now(),
+    ativo boolean
+);
+
+create table perfil_permissao (
+    id_perfil bigint,
+    id_permissao bigint,
+    data_criacao timestamp default now(),
+    ativo boolean,
+    primary key (id_perfil, id_permissao),
+    foreign key (id_perfil) references perfil(id),
+    foreign key (id_permissao) references permissao(id)
+);
+
+create table tipo_veiculo (
+    id bigserial primary key,
+    nome text,
+    data_criacao timestamp default now(),
+    ativo boolean
+);
+
+create table veiculo (
+    id bigserial primary key,
+    id_usuario bigint,
+    id_tipo_veiculo bigint,
+    placa text,
+    modelo text,
+    cor text,
+    data_criacao timestamp default now(),
+    ativo boolean,
+    foreign key (id_usuario) references usuario(id),
+    foreign key (id_tipo_veiculo) references tipo_veiculo(id)
+);
+
+create table usuario_veiculo (
+    id_usuario bigint,
+    id_veiculo bigint,
+    data_criacao timestamp default now(),
+    ativo boolean,
+    primary key (id_usuario, id_veiculo),
+    foreign key (id_usuario) references usuario(id),
+    foreign key (id_veiculo) references veiculo(id)
+);
+
+create table avaliacao (
+    id bigserial primary key,
+    id_usuario bigint,
+    id_veiculo bigint,
+    nota numeric(2,1),
+    data_criacao timestamp default now(),
+    ativo boolean,
+    foreign key (id_usuario) references usuario(id),
+    foreign key (id_veiculo) references veiculo(id)
+);
